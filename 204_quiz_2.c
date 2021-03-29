b0e21c76c444f9c4f8102a342387f0266ae006af
saudrey <weerawutearth@gmail.com>
2021-03-29 17:09:43 +0700

    ลูบไล้ (looblike)

---
+++ 204_quiz_2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j;
+
+    scanf("%d",&n);
+
+    int z[n];
+
+    for ( i = 0; i <n ; i++)
+    {
+       scanf("%d",&z[i]);
+    }
+    
+    for ( i = 0; i < n; i++)
+    {
+        j=1;
+        if (z[i]==j)
+        {
+            
+        }
+        
+        
+    }
+    
+
+}
