e4a7a02b6f948f47b521c59c0e82eec536185bb7
saudrey <weerawutearth@gmail.com>
2021-03-29 18:18:20 +0700

    c

--- 204_quiz_1.c
+++ 204_quiz_1.c
@@ -5,33 +5,14 @@ int main()
     int n,k,c;
 
     scanf("%d %d",&n,&c);
-    int i,z[5],a,j=1;
-    do
+    int i,z[5][10],x[10],a,j=1,l,b;
+    for ( i = 0; i <10; i++)
     {
-        k=n;
-        for ( i = 0; i < 5; i++)
-        {   
-            z[i]=n%10;
-            n=n/10;
-        }
+        x[i]=n;
+        n++;
 
-        for ( i = 0; i < 5; i++)
-        {
-            if (z[i]==c)
-            {
-                printf("%d",k);
-                j=0;
-            }
-            else
-            {
-                n++;
-            }
-            
-            
-        }
-        
-    } while (j==0);
-    
+    }
+  
  
     
 
