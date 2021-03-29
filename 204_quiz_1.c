090045a26686161a960bb1f974d7ed34890c6680
saudrey <weerawutearth@gmail.com>
2021-03-29 17:52:08 +0700

    c

--- 204_quiz_1.c
+++ 204_quiz_1.c
@@ -5,11 +5,34 @@ int main()
     int n,k,c;
 
     scanf("%d %d",&n,&c);
-
-    if (n>0&&n<32750)
+    int i,z[5],a,j=1;
+    do
     {
-        /* code */
-    }
+        k=n;
+        for ( i = 0; i < 5; i++)
+        {   
+            z[i]=n%10;
+            n=n/10;
+        }
+
+        for ( i = 0; i < 5; i++)
+        {
+            if (z[i]==c)
+            {
+                printf("%d",k);
+                j=0;
+            }
+            else
+            {
+                n++;
+            }
+            
+            
+        }
+        
+    } while (j==0);
+    
+ 
     
 
 
